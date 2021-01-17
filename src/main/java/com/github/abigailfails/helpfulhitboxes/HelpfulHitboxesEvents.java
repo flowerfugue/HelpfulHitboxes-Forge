fd9a508a0ec9c32d09163484a0b990383121f85d
abigailfails <thatvaliantvole@gmail.com>
2021-01-17 19:55:06 +0000

    Updated license and issue links

--- src/main/java/com/github/abigailfails/helpfulhitboxes/HelpfulHitboxesEvents.java
+++ src/main/java/com/github/abigailfails/helpfulhitboxes/HelpfulHitboxesEvents.java
@@ -4,6 +4,7 @@ import net.minecraft.block.AbstractRailBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.Item;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
@@ -18,25 +19,24 @@ public class HelpfulHitboxesEvents {
     public static void onPlaceBlock(PlayerInteractEvent.RightClickBlock event) {
         World world = event.getWorld();
         BlockPos pos = event.getPos();
+        Item item = event.getItemStack().getItem();
         BlockState originalState = world.getBlockState(pos);
-        if (originalState.getBlock() instanceof AbstractRailBlock) {
+        if (originalState.getBlock() instanceof AbstractRailBlock && item instanceof BlockItem && ((BlockItem) item).getBlock() instanceof AbstractRailBlock) {
             Direction direction = event.getPlayer().getHorizontalFacing();
-            BlockItemUseContext context = new BlockItemUseContext(event.getPlayer(), event.getHand(), event.getItemStack(), event.getHitVec());
             BlockPos.Mutable currentPos = event.getPos().toMutable().move(direction);
             for (int i = 0; i < 7; i++) {
                 BlockPos nextPos = null;
                 boolean isNextRail = false;
                 BlockPos.Mutable yCheckingPos = currentPos.toMutable().move(Direction.DOWN);
                 for (int j = 0; j < 3; j++) {
-                    if (world.getBlockState(yCheckingPos).isIn(originalState.getBlock())) {
+                    if (world.getBlockState(yCheckingPos).getBlock() instanceof AbstractRailBlock) {
                         nextPos = yCheckingPos.move(direction).toImmutable();
                         isNextRail = true;
-                        //Needs to check for valid diag position too, another variable?
                     } else if (!isNextRail) {
-                        context = new BlockItemUseContext(event.getPlayer(), event.getHand(), event.getItemStack(), event.getHitVec().withPosition(yCheckingPos));
+                        BlockItemUseContext context = new BlockItemUseContext(event.getPlayer(), event.getHand(), event.getItemStack(), event.getHitVec().withPosition(yCheckingPos));
                         if (world.getBlockState(yCheckingPos).isReplaceable(context)) {
                             BlockState stateForPlacement = originalState.getBlock().getStateForPlacement(context);
-                            if (stateForPlacement != null && stateForPlacement.isValidPosition(world, yCheckingPos)) //mIGHT BE THE WRONG CONTEXT
+                            if (stateForPlacement != null && stateForPlacement.isValidPosition(world, yCheckingPos))
                             nextPos = yCheckingPos.toImmutable();
                         }
                     }
@@ -44,8 +44,8 @@ public class HelpfulHitboxesEvents {
                 }
                 if (!isNextRail) {
                     if (nextPos != null) {
-                        context = new BlockItemUseContext(event.getPlayer(), event.getHand(), event.getItemStack(), event.getHitVec().withPosition(nextPos));
-                        ((BlockItem) originalState.getBlock().asItem()).tryPlace(context);
+                        BlockItemUseContext context = new BlockItemUseContext(event.getPlayer(), event.getHand(), event.getItemStack(), event.getHitVec().withPosition(nextPos));
+                        ((BlockItem) item).tryPlace(context);
                         event.setCancellationResult(ActionResultType.SUCCESS);
                         event.setCanceled(true);
                     }
