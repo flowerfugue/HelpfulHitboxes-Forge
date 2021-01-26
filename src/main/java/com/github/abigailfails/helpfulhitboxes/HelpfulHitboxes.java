3db36e26b0b481a2f0134210fe30905d412c5a7a
abigailfails <thatvaliantvole@gmail.com>
2021-01-26 11:16:03 +0000

    Moved rail stuff to Modicum

--- src/main/java/com/github/abigailfails/helpfulhitboxes/HelpfulHitboxes.java
+++ src/main/java/com/github/abigailfails/helpfulhitboxes/HelpfulHitboxes.java
@@ -5,8 +5,4 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(value = HelpfulHitboxes.MOD_ID)
 public class HelpfulHitboxes {
     public static final String MOD_ID = "helpfulhitboxes";
-
-    /*public HelpfulHitboxes() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-    }*/
 }
