3be597ae25bcc4719fc862b5c04cb520a6abf3b9
abigailfails <thatvaliantvole@gmail.com>
2021-01-26 16:58:23 +0000

    Added fabric and forge versions in their own folders

--- src/main/java/com/github/abigailfails/helpfulhitboxes/HelpfulHitboxes.java
+++
@@ -1,8 +0,0 @@
-package com.github.abigailfails.helpfulhitboxes;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(value = HelpfulHitboxes.MOD_ID)
-public class HelpfulHitboxes {
-    public static final String MOD_ID = "helpfulhitboxes";
-}
